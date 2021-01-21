5387a8ee73bb7d35f26d01227362e22bdeecf1d5
Jjurgita <jurgitabalinaite@gmail.com>
2021-01-21 18:07:17 +0200

    GETTER/SETTER notes

---
+++ src/p06/notes/GetterAndSetter.java
@@ -0,0 +1,43 @@
+package p06.notes;
+
+public class GetterAndSetter {
+    // Paprastai klasės laukai yra privatūs, o jų reikšmės nustatomos ir nuskaitomos metodų geteriu ir seteriu pagalba
+    /*
+    Geteriai ir seteriai pavadinimai paprastai sudaryti iš lauko pavadinimo priekyje pridedant set ir get ir lauko pavadinimą užrašant iš didžiosios raidės.
+    Galima išimtis, kai laukas yra boolean tipo, tai tada tokio lauko geteris pradedamas žodeliu is.
+     */
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Geteris neturi parametrų ir gražiną tokio pat tipo reikšmę kaip atitinkamas laukas
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    //Seteris turi vieną parametrą tokio pat tipo kaip atitinkamas laukas ir negrąžina jokios reikmės, t.y. void
+    private int age;
+    private boolean live;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setLive(boolean live) {
+        this.live = live;
+    }
+
+    //boolean tipo lauko geteris pradedamas žodeliu is, yra be parametru ir grazina tokio pat tipo reiksme
+    private boolean live;
+
+    public boolean isLive() {
+        return live;
+    }
+}
