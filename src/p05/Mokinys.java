c873e684247ed5f45210f7ebefd0c16e1d59c66e
Jjurgita <jurgitabalinaite@gmail.com>
2021-01-21 15:21:01 +0200

    "Mokinys" task in progress

--- src/p05/Mokinys.java
+++ src/p05/Mokinys.java
@@ -1,6 +1,25 @@
 package p05;
 
 public class Mokinys {
-    String name;
-    int level;
+    //jeigu Mokinys class file butu kitame package reikru prie Sring, int ir Mokinys konstruktoriaus prideti "public"
+    String firstname; // pagal nutylejima bus = null
+    String lastname; // = null
+    int level; // = 0
+
+    //metodas-konstruktorius: Java kalboj yra toks pats kaip class name
+//    Mokinys(String n, int l) {
+//        name = n;
+//        level = l;
+//    }
+
+    //jeigu norime kintamuosius vadinti taip pat reikia prideti "this"
+    Mokinys (String firstname, String lastname, int level) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.level = level;
+    }
+    Mokinys (String firstname) {
+        this.firstname = firstname;
+    }
+
 }
--- src/p05/P05U1.java
+++ src/p05/P05U1.java
@@ -5,13 +5,50 @@ import p05.Mokinys;
 public class P05U1 {
     public static void main(String[] args) {
 
+        //Tarkime turime masyvą objektų Mokinys. Reikia atspausdinti mokinių vardus ir pavardes surūšiuotus pagal klases ir pagal pavardes bei vardus.
+        //Pastaba: nustatykite keliems iš vienos klasės mokiniams tas pačias pavardes bet skirtingus vardus, kad patikrinti ar gerai rūšiuoja.
+
         //susikurti Mokiniu masyva nurodant pilna kelia ir class pavadinima
         //taip rasyti nera visad patogu, taciau galima
         //p05.Mokinys[] mokiniai = new p05.Mokinys[5];
 
         //jeigu nenorime rasyti ilgu pavadinimu, reikia import pradzioje class kelia
-        Mokinys[] mokiniai = new Mokinys[5];
+        Mokinys[] mokiniai = new Mokinys[]{
+                new Mokinys("Antanas", "Pertrauskas", 1),
+                new Mokinys("Jonas", "Sirtautas", 2),
+                new Mokinys("Ona", "Onaite", 3),
+                new Mokinys("Kazimieras", "Penktadienis", 4),
+                new Mokinys("Egle", "Liepaite", 1),
+                new Mokinys("Ruta", "Jonaite", 3),
+                new Mokinys("Antanas", "Antanaitis", 4),
+                new Mokinys("Saule", "Liepaite", 1),
+                new Mokinys("Ugne", "Jonaite", 3)
+        };
 
+        //spausdinam pilna sukurta mokiniu sarasa
+        System.out.println("Pirminiai duomenys: ");
+        print(mokiniai);
 
+        //sulygiuoti pagal klase
+        for (int i = 0; i < mokiniai.length - 1 ; i++) {
+            for (int j = i + 1; j < mokiniai.length; j++){
+                if (mokiniai[i].level > mokiniai[j].level) {
+                    //susikuriam laikina kintamaji tmp
+                    // i <-> j (atliekam kaip su tom dviem stiklinem - perpilt reikia triju stikliniu)
+                    Mokinys tmp = mokiniai[i];
+                    mokiniai[i] = mokiniai[j];
+                    mokiniai[j] = tmp;
+                }
+            }
+        }
+        System.out.println("Surusiuota info: ");
+        print(mokiniai);
+    }
+
+    //apsirasom metoda print, kad nereiktu vis to paties rasyti - reiks prideti tik "print(mokiniai);"
+    static void print(Mokinys[] mokiniai) {
+        for (Mokinys m : mokiniai) {
+            System.out.println(m.firstname + " " + m.lastname + " " + m.level);
+        }
     }
 }
