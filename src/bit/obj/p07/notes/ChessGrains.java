dccff13677e809810b47669d02a550d8ad693688
Jjurgita <jurgitabalinaite@gmail.com>
2021-01-27 10:23:49 +0200

    Chess and Grains task

--- src/bit/obj/p07/notes/ChessGrains.java
+++ src/bit/obj/p07/notes/ChessGrains.java
@@ -3,7 +3,13 @@ package bit.obj.p07.notes;
 import java.math.BigInteger;
 
 public class ChessGrains {
+    //į pirmą šachmatų lentos laukelį reikėjo padėti 1 grūdą,
+    //į antrą 2,
+    //į trečią 2 x 2 = 4,
+    //į ketvirtą 4 x 2 = 8,
+    //į penktą – 8 x 2 = 16 ir taip iki 64 langelio..
 
+    //su funkcija:
 //    public static void main (String[] args)
 //    {
 //        grainCounter(64);
@@ -19,12 +25,16 @@ public class ChessGrains {
 //        }
 //    }
 
+    //geresnis budas:
     public static void main(String[] args) {
-        BigInteger grains = BigInteger.valueOf(2);
+        BigInteger grains = BigInteger.ONE;
+        BigInteger grainsSum = BigInteger.valueOf(1);
+
         for (int i=1; i < 65; i++)
         {
             System.out.println("Square: " + String.format("%d", i) + " - " + String.format("%d", grains));
-            grains = grains.multiply(BigInteger.valueOf(2));
+            grains = grains.multiply(BigInteger.TWO);
+            grainsSum = grains.add(grains);
         }
     }
 }
