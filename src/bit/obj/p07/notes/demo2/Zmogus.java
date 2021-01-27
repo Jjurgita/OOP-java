df3b13440e1596a5448b6e043823e198d9110df0
Jjurgita <jurgitabalinaite@gmail.com>
2021-01-27 16:12:42 +0200

    Static variable and method example

---
+++ src/bit/obj/p07/notes/demo2/Zmogus.java
@@ -0,0 +1,31 @@
+package bit.obj.p07.notes.demo2;
+
+public class Zmogus {
+    private static int count = 0;
+    private static int real = 0;
+    private String vardas;
+    private String pavarde;
+
+    public Zmogus(String vardas) {
+        this.vardas = vardas;
+        count++;
+        real++;
+    }
+
+    public String pilnasVardas() {
+
+        return vardas + (pavarde == null ? "" : ' ' + pavarde);
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Zmogus.count = count;
+    }
+
+    public static int getReal() {
+        return real;
+    }
+}
