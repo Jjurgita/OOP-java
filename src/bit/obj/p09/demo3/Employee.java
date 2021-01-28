5654be293396d746aa3bb1581275d352bd417aaf
Jjurgita <jurgitabalinaite@gmail.com>
2021-01-28 14:28:38 +0200

    Interface (default) examples

---
+++ src/bit/obj/p09/demo3/Employee.java
@@ -0,0 +1,29 @@
+package bit.obj.p09.demo3;
+
+public class Employee implements Salary{
+
+    String name;
+    double salary;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
